c1070e83c67538d6ab01b070ca79b7c45170228b
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-17 14:18:06 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Upload',
+    '{description}' => 'Configure settings for loading resources to the server',
+    '{permissions}' => [
+        'any'  => ['Full access', 'Configuring settings for downloading resources to the server']
+    ],
+
+    // Form: поля
+    'File upload address' => 'File upload address',
+    'Local (base) folder path' => 'Local (base) folder path',
+    'Local (base) URL path' => 'Local (base) URL path',
+    'File upload rules' => 'File upload rules',
+    'Check file extension' => 'Check file extension',
+    'Check MIME type of file contents' => 'Check MIME type of file contents',
+    'No rules for user roles' => 'No rules for user roles',
+    'Checked file extensions' => 'Checked file extensions',
+    'New file name after downloading it' => 'New file name after downloading it',
+    'File name transliteration' => 'File name transliteration',
+    'Creating a unique file name' => 'Creating a unique file name',
+    'Exclude special characters from file name' => 'Exclude special characters from file name',
+    'Replace special characters with' => 'Replace special characters with',
+    'Maximum file name length' => 'Maximum file name length',
+    'Setting PHP directives each time a file is uploaded' => 'Setting PHP directives each time a file is uploaded',
+    'Set PHP directives' => 'Set PHP directives',
+    'Maximum upload file size' => 'Maximum upload file size',
+    'Maximum number of downloads' => 'Maximum number of downloads',
+    'PHP directives configuration to pass maximum data size' => 'PHP directives configuration to pass maximum data size',
+    'Maximum size of data sent by the POST method' => 'Maximum size of data sent by the POST method',
+    'Maximum upload file size' => 'Maximum upload file size',
+    'Maximum allowed number of simultaneously uploaded files' => 'Maximum allowed number of simultaneously uploaded files',
+    'Maximum memory size in bytes that the script can use' => 'Maximum memory size in bytes that the script can use',
+    'Maximum time in seconds to load a script' => 'Maximum time in seconds to load a script',
+    'Using the INTL module to translate file names' => 'Using the INTL module to translate file names',
+    'Allowed to upload files on the server side' => 'Allowed to upload files on the server side',
+    'Lowercase file name' => 'Lowercase file name',
+    // Form: сообщения / заголовки
+    'Save settings' => 'Сохранение настроек',
+    'Reset settings' => 'Сброс настроек',
+    // Form: сообщения / текст
+    'settings saved {0} successfully' => 'успешное сохранение настроек "<b>{0}</b>"',
+    'settings reseted {0} successfully' => 'успешный сброс настроек "<b>{0}</b>"'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Загрузка',
+    '{description}' => 'Параметры загрузки ресурсов на сервер',
+    '{permissions}' => [
+        'any'  => ['Полный доступ', 'Настройка параметров загрузки ресурсов на сервер']
+    ],
+
+    // Form: поля
+    'File upload address' => 'Адрес загрузки файлов',
+    'Local (base) folder path' => 'Локальный (базовый) путь к папке',
+    'Local (base) URL path' => 'Локальный (базовый) URL-путь',
+    'File upload rules' => 'Правила загрузки файла',
+    'Check file extension' => 'Проверять расширение файла',
+    'Check MIME type of file contents' => 'Проверять MIME-тип содержимого файла',
+    'No rules for user roles' => 'Без правил для ролей пользователей',
+    'Checked file extensions' => 'Проверяемые расширения файлов',
+    'New file name after downloading it' => 'Новое имя файла после его загрузки',
+    'File name transliteration' => 'Транслитерация имени файла',
+    'Creating a unique file name' => 'Формирование уникального имени файла',
+    'Exclude special characters from file name' => 'Исключить специальные символы из имени файла',
+    'Replace special characters with' => 'Заменить специальные символы на',
+    'Maximum file name length' => 'Максимальная длина имени файла',
+    'Setting PHP directives each time a file is uploaded' => 'Установка значений директивам PHP при каждой загрузке файла',
+    'Set PHP directives' => 'Устанавливать значения директивам PHP',
+    'Maximum upload file size' => 'Максимальный размер закачиваемого файла',
+    'Maximum number of downloads' => 'Максимальное количество загружаемых файлов',
+    'PHP directives configuration to pass maximum data size' => 'Конфигурация директив PHP для передачи максимально допустимого размера данных',
+    'Maximum size of data sent by the POST method' => 'Макс-о допустимый размер данных, отправляемых методом POST',
+    'Maximum upload file size' => 'Макс-й размер закачиваемого файла',
+    'Maximum allowed number of simultaneously uploaded files' => 'Макс-о разрешенное количество одновременно закачиваемых файлов',
+    'Maximum memory size in bytes that the script can use' => 'Макс-й объем памяти в байтах, который может использовать скрипт',
+    'Maximum time in seconds to load a script' => 'Максимальное время в секундах для загрузки скрипта',
+    'Using the INTL module to translate file names' => 'Использование модуля INTL для транслита имен файлов',
+    'Allowed to upload files on the server side' => 'Разрешено закачивание файлов на стороне сервера',
+    'Lowercase file name' => 'Имя файла в нижнем регистре',
+    'reset settings' => 'сбросить настройки',
+    // Form: сообщения / заголовки
+    'Save settings' => 'Сохранение настроек',
+    'Reset settings' => 'Сброс настроек',
+    // Form: сообщения / текст
+    'settings saved {0} successfully' => 'успешное сохранение настроек "<b>{0}</b>"',
+    'settings reseted {0} successfully' => 'успешный сброс настроек "<b>{0}</b>"'
+];
